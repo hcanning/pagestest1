db1f92b92a6969e18437cf7b3e48eb1610e4b2f8
hcanning <46687863+hcanning@users.noreply.github.com>
2025-06-26 14:54:47 -0400

    Prepare for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/pagestest1',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Put big dependencies in their own chunks
+            if (id.includes('three')) return 'vendor-three';
+            if (id.includes('three-mesh-bvh')) return 'vendor-bvh';
+            if (id.includes('lottie')) return 'vendor-lottie';
+            return 'vendor'; // fallback vendor chunk
+          }
+        },
+      },
+    },
+  },
 });
